68a0d123c7b68f1a070734bc87cf72615ce30433
Massimiliano Pasquini <massimiliano.pasquini@outlook.com>
2024-01-11 10:09:42 +0100

    feat: Translate Command require destination language
    Translate Command now require to specify the destination language

--- src/ollama-translate.tsx
+++ src/ollama-translate.tsx
@@ -1,7 +1,8 @@
 import { AnswerView } from "./api/ui/AnswerView";
+import { RaycastArgumentsOllamaCommandTranslate } from "./api/types";
 
-export default function Command(): JSX.Element {
+export default function Command(props: RaycastArgumentsOllamaCommandTranslate): JSX.Element {
   const c = "translate";
-  const p = "Act as a translator. Translate the following text.\n\nOutput only with the translated text.\n";
+  const p = `Act as a translator. Translate the following text to ${props.arguments.language}.\n\nOutput only with the translated text.\n`;
   return <AnswerView command={c} prompt={p} />;
 }
